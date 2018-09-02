0cbad26df38256f88e19fe5d5dafc016d6ea7248
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2018-09-02 12:49:31 +0200

    UPDATE - changed mail to message

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+const socket = io();
+socket.on('connect', () => {
+    console.log('Connected to server');
+    socket.emit('createMessage', {
+        from: 'Andrew',
+        text: 'Hey, this works for me'
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+});
+socket.on('newMessage', message => {
+    console.log('New message', message);
+});
--- server/server.js
+++ server/server.js
@@ -9,13 +9,22 @@ const app = express();
 
 app.use(express.static(publicPath));
 const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+});
+
 const io = socketIO(server);
 io.on('connection', socket => {
     console.log('New user connected!');
+    socket.emit('newMessage', {
+        from: 'John',
+        text: 'See you then',
+        createdAt: 1234
+    });
+    socket.on('createMessage', message => {
+        console.log('createMessage', message);
+    });
 });
 io.on('disconnect', socket => {
     console.log('User was disconnected');
-});
-server.listen(port, () => {
-    console.log(`Server is up on ${port}`);
 });
