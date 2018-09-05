735c0a6d36e36ec5c6de63affeaca09fb5b9632a
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2018-09-05 21:17:56 +0200

    ADD  - message confirmation

--- public/js/index.js
+++ public/js/index.js
@@ -8,4 +8,28 @@ socket.on('disconnect', () => {
 });
 socket.on('newMessage', message => {
     console.log('New message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+socket.emit(
+    'createMessage',
+    {
+        from: 'Frank',
+        text: 'Hi'
+    },
+    message => console.log('got it', { message })
+);
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit(
+        'createMessage',
+        {
+            from: 'User',
+            text: jQuery('[name=message]').val()
+        },
+        function() {}
+    );
 });
