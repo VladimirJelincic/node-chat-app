51ad91609a38d12051a9c62691fffd916cee8b9e
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2018-09-06 20:54:04 +0200

    ADD geolocation api call

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,15 @@ socket.on('newMessage', message => {
     li.text(`${message.from}: ${message.text}`);
     jQuery('#messages').append(li);
 });
+socket.on('newLocationMessage', function(message) {
+    console.log('location', message);
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location </a>');
+    li.text(`${message.from}`);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
 socket.emit(
     'createMessage',
     {
@@ -32,4 +41,22 @@ jQuery('#message-form').on('submit', function(e) {
         },
         function() {}
     );
+});
+var locationButton = jQuery('#send-location');
+locationButton.on('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported  by your browser');
+    } else {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                socket.emit('createLocationMessage', {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            },
+            () => {
+                alert('Unable to fetch location.');
+            }
+        );
+    }
 });
